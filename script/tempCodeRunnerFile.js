9fbe991ac7aa1fe87000d8ece4057a16e077770d
im-lakshyaa <lakshya20042004@gmail.com>
2025-09-17 22:38:56 +0530

    week-4 proggres

---
+++ script/tempCodeRunnerFile.js
@@ -0,0 +1,20 @@
+//hof practice
+// function applyTwice(double,x){
+//     return double(double(x));
+// }
+// function double(x){
+//     return x*2;
+// }
+// console.log(applyTwice(double,10));
+
+//next question
+
+let arr=[1,2,3,4,5]
+function filter(array,x){
+    return evens(x)
+}
+function evens(x){
+    if(x%2==0);
+    return x;
+}
+console.log(filter(arr,4));
