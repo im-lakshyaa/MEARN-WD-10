e1304db11232f3642b6efcf88f9ecd100558f932
im-lakshyaa <lakshya20042004@gmail.com>
2025-09-25 17:04:39 +0530

    react-start

---
+++ nodejs/filesystem.js
@@ -0,0 +1,46 @@
+import { log } from "console";
+import fs from "fs";
+//writefile operation...
+// fs.writeFileSync("lakshya.pdf","hello world");
+// fs.writeFileSync("lakshya.pdf","job chiye");
+// fs.appendFileSync("lakshya.pdf","\n job abhi bhi nhi mili");
+
+//read operation
+// const data=fs.readFileSync("lakshya.pdf","utf-8");
+// console.log(data);
+
+// fs.writeFile("abc.pdf","lakshya",(err)=>{
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("file is created");    
+//     }
+// })
+
+// fs.writeFile("abc.pdf","bhai job jaldi lagegi",(err)=>{
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("file have some changes");
+        
+//     }
+// })
+// fs.appendFile("abc.pdf","interview dena jayenga",(err)=>{
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("\n file have some changes");
+        
+//     }
+// })
+
+// const data=fs.readFile("abc","kuch toh hua h",(err)=>{
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log(data);    
+//     }
+// })
+
+
+
---
+++ nodejs/index.js
@@ -0,0 +1,27 @@
+// function sayhi(api){
+//     return `hi,${sayhi}`;
+// }
+
+// function add(a,b){
+//     return a+b;
+// }
+// function sub(a,b){
+//     return a-b;
+// }
+// function multiply(a,b){
+//     return a*b;
+// }
+// function divide(a,b){
+//     return a/b;
+// }
+// export{add,sub,multiply,divide}
+// function api(){
+//     fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
+// }
+// export default api;
+//api to ui maker
+
+
+console.log("hello world");
