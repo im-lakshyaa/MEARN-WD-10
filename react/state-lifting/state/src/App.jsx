999a03cbfd900869c7ef1dd1581190974301e599
im-lakshyaa <lakshya20042004@gmail.com>
2025-09-26 14:39:56 +0530

    props , state lifting

---
+++ react/state-lifting/state/src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import { useState } from 'react'
+
+const App = () => {
+  const [name,setname]= useState{
+    return ()
+  }
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
+
